6c7b3c1a0fb58b40ef5902cf0682a8a591a931f2
brunajsilv <brunajs@outlook.com.br>
2024-10-30 18:56:47 -0300

    Codigos Bootcamp Wex

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Ao abrir um projeto novo no C# dentro de pastas já criadas em um local do seu computador e também  com as extensõe aplicadas, a IDEA apresenta essa mensagem de "Hello, Word".
