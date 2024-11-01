f39fdec356f1cd80eb3f11a16fd431f95928e36f
brunajsilv <brunajs@outlook.com.br>
2024-10-31 21:30:27 -0300

    Adiciona classe Calculadora e programa para executá-la

---
+++ Models/Calculadora.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Criaçao de classe calculadora
+
+//Operacões mtématicas
+namespace Codigos.Models
+{
+    public class Calculadora
+    {
+        public void Soma(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }    
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }    
+        public void Mltiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }    
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }    
+    }
+}
---
+++ ProgramacCalculador.cs
@@ -0,0 +1,10 @@
+using Codigos.Models;
+
+//criando as variaveis: 
+
+Calculadora calc = new Calculadora(); //calc é a variavel 
+
+calc.Soma(10,30);
+calc.Subtrair(20,30);
+calc.Mltiplicar(10,30);
+calc.Dividir(30,30);
